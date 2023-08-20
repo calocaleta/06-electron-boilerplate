603d334e09bde53e8820ae2d8180e1326d14c669
Carlos A. Garcia Gonzales <cgarcia@stsac.com>
2023-08-19 21:30:21 -0500

    Primer commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow, dialog } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  win.webContents.openDevTools();
+  win.setMenu(null);
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
