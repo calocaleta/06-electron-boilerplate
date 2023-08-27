e0f1b065b06616377d5458f8f9205a39a916dce6
Carlos A. Garcia Gonzales <cgarcia@stsac.com>
2023-08-27 18:38:02 -0500

    Se mejoro la interface gráfica

--- electron/windows/mainWindow/index.js
+++ electron/windows/mainWindow/index.js
@@ -4,14 +4,17 @@ const path = require('path');
 function createMainWindow() {
   const win = new BrowserWindow({
     width: 800,
-    height: 600,
+    width: 800,
+    height: 595,
+    icon: path.join(__dirname, 'icon.png'),
+    resizable: false,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
       preload: path.join(__dirname, '../../preload.js')
     }
   });
-  win.webContents.openDevTools();
+  //win.webContents.openDevTools();
   win.setMenu(null);
   win.loadFile('./public/index.html');
 }
--- src/utils/Instalation.jsx
+++ src/utils/Instalation.jsx
@@ -254,8 +254,8 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
     return (
-    <div className="bg-slate-600">
-    <h1>APP ELECTRON!</h1>
+    <div className="w-full h-screen m-0 p-0 flex flex-col">
+        <h1>APP ELECTRON!</h1>
     </div>
     )
 };
@@ -279,7 +279,9 @@ dist/`,selectedPath);
 }`,selectedPath);
         makeFile('tailwind.config.js', `/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/*.html', './src/*.js', './src/*.jsx', './src/*.tsx',
+    content: [
+            './public/*.html', './public/*.js',
+            './src/*.html', './src/*.js', './src/*.jsx', './src/*.tsx',
             './src/**/*.html', './src/**/*.js', './src/**/*.jsx', './src/**/*.tsx'],
     theme: {
     extend: {},
@@ -350,20 +352,68 @@ await executeCommand('code .', selectedPath);
     }, []);
 
     return (
-        <div>
-            <button
-                className="bg-blue-500 border border-blue-700 text-white rounded px-4 py-2 focus:outline-none active:bg-blue-700 active:border-blue-900" 
-                onClick={openDialog}>
-                    Seleccionar Directorio
-            </button>
+        <div className="w-full h-screen m-0 p-0 flex flex-col">
 
-            <p>Directorio seleccionado: - {selectedPath} -</p>
 
-            <button
-                className="bg-blue-500 border border-blue-700 text-white rounded px-4 py-2 focus:outline-none active:bg-blue-700 active:border-blue-900" 
-                onClick={onClick}>
-                    Ejecuta comando!
-            </button>
+        <div className="relative bg-gray-900">
+            <div className="relative h-80 overflow-hidden bg-indigo-600 md:absolute md:left-0 md:h-full md:w-1/3 lg:w-1/2">
+                <img
+                className="h-full w-full object-cover object-left"
+                src="background.jpg"
+                alt=""
+                />
+                <svg
+                viewBox="0 0 926 676"
+                aria-hidden="true"
+                className="absolute -bottom-24 left-24 w-[57.875rem] transform-gpu blur-[118px]"
+                >
+                <path
+                    fill="url(#60c3c621-93e0-4a09-a0e6-4c228a0116d8)"
+                    fillOpacity=".4"
+                    d="m254.325 516.708-90.89 158.331L0 436.427l254.325 80.281 163.691-285.15c1.048 131.759 36.144 345.144 168.149 144.613C751.171 125.508 707.17-93.823 826.603 41.15c95.546 107.978 104.766 294.048 97.432 373.585L685.481 297.694l16.974 360.474-448.13-141.46Z"
+                />
+                <defs>
+                    <linearGradient
+                    id="60c3c621-93e0-4a09-a0e6-4c228a0116d8"
+                    x1="926.392"
+                    x2="-109.635"
+                    y1=".176"
+                    y2="321.024"
+                    gradientUnits="userSpaceOnUse"
+                    >
+                    <stop stopColor="#776FFF" />
+                    <stop offset={1} stopColor="#FF4694" />
+                    </linearGradient>
+                </defs>
+                </svg>
+            </div>
+            <div className="relative mx-auto max-w-7xl py-24 sm:py-32 lg:px-8 lg:py-40">
+                <div className="pl-6 pr-6 md:ml-auto md:w-2/3 md:pl-16 lg:w-1/2 lg:pl-24 lg:pr-0 xl:pl-32">
+                <h2 className="text-base font-semibold leading-7 text-indigo-400">Visual Electron 1.0.0</h2>
+                <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Two Step Installation</p>
+                <p className="mt-6 text-base leading-7 text-gray-300">
+                    This installer will let you set up a clean ElectronJS installation in just two easy steps, complete with libraries for file access, command execution, dialog box management, and even AI API connections. Save time and focus on what really matters - writing code!
+                </p>
+                <div className="mt-8">
+                    <a
+                    href="#"
+                    className="inline-flex rounded-md bg-white/10 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-white/20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+                    onClick={openDialog}
+                    >
+                    Select your project folder
+                    </a>
+                    <a
+                    href="#"
+                    className="ml-5 inline-flex rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    onClick={onClick}
+                    >
+                    Install Electron
+                    </a>
+                    <p className='text-white'>Current Path: {selectedPath} </p>
+                </div>
+                </div>
+            </div>
+            </div>
             
         </div>
     )
