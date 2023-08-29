53af295ea84b2c756f00e6e560f4b2fe59c21d1b
Carlos A. Garcia Gonzales <cgarcia@stsac.com>
2023-08-28 23:28:42 -0500

    Instalador EXE

--- electron/windows/mainWindow/index.js
+++ electron/windows/mainWindow/index.js
@@ -4,16 +4,21 @@ const path = require('path');
 function createMainWindow() {
   const win = new BrowserWindow({
     width: 800,
-    width: 800,
-    height: 595,
+    height: 620,
     icon: path.join(__dirname, 'icon.png'),
     resizable: false,
+    show: false,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
       preload: path.join(__dirname, '../../preload.js')
     }
   });
+
+  win.once('ready-to-show', () => {
+    win.show();
+  });
+
   //win.webContents.openDevTools();
   win.setMenu(null);
   win.loadFile('./public/index.html');
